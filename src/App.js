c0c1c23a726549b54370f05d2a76cd2016237285
Naveed <naveedimran.pinjari@omniwyse.com>
2022-05-06 17:24:28 +0530

    basic project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import {Table} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useEffect, useState} from "react";
+import orders from "./orders";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+    const [itemData, setItemData] = useState({});
+    useEffect(() => {
+        setItems(orders);
+        if (orders.length > 0) {
+            setItemData(orders[0]);
+        }
+    }, []);
+    return (
+        <div className="App">
+            <h1>Summary</h1>
+            <div className="tab">
+                <h3>Orders</h3>
+            <div className="box">
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Date</th>
+                        <th>Order Name</th>
+                        <th>Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        items.map((item, index) => {
+                            return (
+                                <tr onClick={() => setItemData(item)}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.date}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.amount}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </Table>
+            </div>
+            </div>
+            <div>
+                <h3>Order Data</h3>
+                {
+                    itemData !== {} &&
+                    <div className="tab">
+                        <div>Name : {itemData.name}</div>
+                        <div>Date : {itemData.date}</div>
+                        <div>Total Amount : {itemData.amount}</div>
+                        {
+                            itemData.items && itemData.items.length > 0 &&
+                            <div className="box">
+                                <Table striped bordered hover>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Item Name</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        itemData.items.map((item, index) => {
+                                            return (
+                                                <tr>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.itemName}</td>
+                                                    <td>{item.price}</td>
+                                                    <td>{item.quantity}</td>
+                                                    <td>{item.total}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                    </tbody>
+                                </Table>
+                            </div>
+                        }
+
+
+                    </div>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/orders.js
@@ -0,0 +1,238 @@
+const orders = [
+    {
+      name: "monday order",
+      amount: "$112",
+      date: "03/05/2022",
+      items: [
+        {
+          itemName : "oranges",
+          quantity : 2,
+          price : "$2",
+          total : "$4"
+        }
+      ]
+    },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "monday order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "monday order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "monday order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "monday order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "monday order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "tuesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+  {
+    name: "wednesdays order",
+    amount: "$112",
+    date: "03/05/2022",
+    items: [
+      {
+        itemName : "oranges",
+        quantity : 2,
+        price : "$2",
+        total : "$4"
+      }
+    ]
+  },
+];
+
+export default orders;
